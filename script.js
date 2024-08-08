6e30737865e0269e794f1433638ac631bd741c8b
edunovacki <111133454+edunovacki@users.noreply.github.com>
2024-08-08 07:44:11 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,9 +6,20 @@ const caixaResultado = document.querySelector('.caixa-resultado');
 const perguntas = [//abre a lista de objetos (itens)
     {//abre o item
         enunciado:"Pergunta 1",
-        alternativas:[
-            "Alternativa 1",
-            "Alternativa 2"
-        ]
+        alternativas:["Alternativa 1","Alternativa 2"]
+    },
+    {
+        enunciado:"Pergunta 2",
+        alternativas:["Alternativa 1","Alternativa 2"]
+    },
+    {
+        enunciado:"Pergunta 3",
+        alternativas:["Alternativa 1","Alternativa 2"]
     }
 ]
+let posicao = 0;
+let perguntaAtual; 
+
+function mostraPergunta(){
+    
+}
